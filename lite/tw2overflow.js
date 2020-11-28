ec8df69d469238bd8c29a03706798474d291a9d8
SebaBosssU <rohanonlineseboss@gmail.com>
2020-11-28 23:36:55 +0100

    x
    x

--- lite/tw2overflow.js
+++ lite/tw2overflow.js
@@ -1,6 +1,6 @@
 /*!
  * tw2overflow v2.0.0
- * Sat, 28 Nov 2020 22:29:15 GMT
+ * Sat, 28 Nov 2020 22:34:08 GMT
  * Developed by Relaxeaza <twoverflow@outlook.com>
  *
  * This work is free. You can redistribute it and/or modify it under the
@@ -6828,10 +6828,6 @@ define('two/battleCalculator', [
                 previllageSword = data.available_units.sword.total
                 previllageTrebuchet = data.available_units.trebuchet.total
             })
-            setUnits()
-        }
-
-        function setUnits() {
             villageAxe = previllageAxe
             villageArcher = previllageArcher
             villageCatapult = previllageCatapult
